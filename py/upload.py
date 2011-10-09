f36e36a71ca00b2929ed6e70072296539815981a
Christoph Schaefer <cppschaefer@gmail.com>
2011-10-09 15:18:11 +0200

    first commit

---
+++ py/upload.py
@@ -0,0 +1,25 @@
+from mod_python import apache
+from mod_python import Session
+from mod_python import util
+import Image
+import subprocess
+
+doctype = "<!doctype html>"
+header = "<head><meta charset=utf-8><title>Vectorize</title></head>"
+
+def result(req, img = None):
+  session = Session.Session(req)
+  session.save()
+  try:
+    image = Image.open(img.file)
+    image_type = image.format
+  except:
+    util.redirect(req, '/error_page.html')
+  if image_type != 'JPEG' and image_type != 'PNG' and image_type != 'BMP':
+    util.redirect(req, '/error_page.html')
+  else:    
+    image.thumbnail((250,250))
+    image.save('/home/ubuntu1/www/img/' + session.id() + '.bmp', "BMP")
+    (width, height) = image.size
+    subprocess.call('cat /home/ubuntu1/www/img/' + session.id() + '.bmp | mkbitmap -f 4 -s 2 -t 0.45 | potrace -t 5 -s > /home/ubuntu1/www/img/' + session.id() + '.svg', shell=True)
+    return "{0:s}<html>{1:s}<body><img src='/img/{2:s}.bmp' width='{3:s}' height='{4:s}' /><img src='/img/{2:s}.svg' width='{3:s}' height='{4:s}' /></body></html>".format(doctype, header, session.id(), str(width), str(height))
