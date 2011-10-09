50c6237f215fe6dbd0ea41d442a1c960c9f47a2a
Christoph Schaefer <cppschaefer@gmail.com>
2011-10-09 17:06:44 +0200

    Explanation in README added, renamings

--- py/upload.py
+++ py/upload.py
@@ -4,8 +4,9 @@ from mod_python import util
 import Image
 import subprocess
 
-doctype = "<!doctype html>"
-header = "<head><meta charset=utf-8><title>Vectorize</title></head>"
+doctype = '<!doctype html>'
+header = '<head><meta charset=utf-8><title>Vectorize</title></head>'
+home_dir = '/home/ubuntu1/Vectorize-Gallery'
 
 def result(req, img = None):
   session = Session.Session(req)
@@ -19,7 +20,7 @@ def result(req, img = None):
     util.redirect(req, '/error_page.html')
   else:    
     image.thumbnail((250,250))
-    image.save('/home/ubuntu1/www/img/' + session.id() + '.bmp', "BMP")
+    image.save(home_dir + '/tmp/' + session.id() + '.bmp', "BMP")
     (width, height) = image.size
-    subprocess.call('cat /home/ubuntu1/www/img/' + session.id() + '.bmp | mkbitmap -f 4 -s 2 -t 0.45 | potrace -t 5 -s > /home/ubuntu1/www/img/' + session.id() + '.svg', shell=True)
-    return "{0:s}<html>{1:s}<body><img src='/img/{2:s}.bmp' width='{3:s}' height='{4:s}' /><img src='/img/{2:s}.svg' width='{3:s}' height='{4:s}' /></body></html>".format(doctype, header, session.id(), str(width), str(height))
+    subprocess.call('cat ' + home_dir + '/tmp/' + session.id() + '.bmp | mkbitmap -f 4 -s 2 -t 0.45 | potrace -t 5 -s > ' + home_dir + '/tmp/' + session.id() + '.svg', shell=True)
+    return "{0:s}<html>{1:s}<body><img src='/tmp/{2:s}.bmp' width='{3:s}' height='{4:s}' /><img src='/tmp/{2:s}.svg' width='{3:s}' height='{4:s}' /></body></html>".format(doctype, header, session.id(), str(width), str(height))
