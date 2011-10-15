d0d3747b6e7bcd2fdc1b2a4061ecd85c47135d96
Christoph Schaefer <cppschaefer@gmail.com>
2011-10-15 20:13:48 +0200

    add URL parameters for dynamic image generation

--- py/img.py
+++ py/img.py
@@ -7,6 +7,29 @@ def index(req, img = None):
   return "index"
 
 def img(req, session_id = None, image_id = None):
+  commands = image_id.split("-")
+
+highpass_filter = commands[0]
+scale_factor = commands[1]
+if commands[2] = "c":
+  scale_method = "--cubic"
+else:
+  scale_method = "--linear"
+threshold = "0." + commands[3]
+if commands[4] = "y":
+  invert = "--invert"
+else:
+  invert = ""
+turnpolicy = commands[5]
+turdsize = commands[6]
+alphamax = "1." + commands[7]
+foreground_color = "#" + commands[8]
+if commands[9] = "y":
+  opaque_background = "--opaque"
+  background_color = "#" + commands[10]
+else:
+  invert = ""
+  background_color = ""
 
   cat_output = subprocess.Popen(["cat", tmp_dir + session_id + ".bmp"], stdout=subprocess.PIPE)
   mkbitmap_output = subprocess.Popen(["mkbitmap", "--filter", "4", "--scale", "1", "--threshold", "0.35"], stdin=cat_output.stdout, stdout=subprocess.PIPE)
--- py/upload.py
+++ py/upload.py
@@ -6,8 +6,8 @@ import subprocess
 import urllib
 import os
 
-absolut_path = '/home/ubuntu1/Vectorize-Gallery/'
-relativ_upload_path = 'tmp/'
+absolut_image_path = '/home/ubuntu1/Vectorize-Gallery/tmp/'
+max_image_size = (250,250)
 
 def result(req, img = None):
   session = Session.Session(req)
@@ -20,58 +20,37 @@ def result(req, img = None):
   if image_type != 'JPEG' and image_type != 'PNG' and image_type != 'BMP':
     util.redirect(req, '/error_page.html')
   else:    
-    image.thumbnail((250,250))
-    image_path_prefix = session.id()    
-    os.chdir(absolut_path + relativ_upload_path)
-    image.save('{0:s}.bmp'.format(image_path_prefix), 'BMP')
-    all_pathes = _generate_svgs(image_path_prefix)
+    image.thumbnail(max_image_size)
+    image_id = session.id()    
+    image.save('{0:s}{1:s}.bmp'.format(absolut_image_path, image_id), 'BMP')
+    all_pathes = _generate_svg_pathes(image_id)
     return _get_html_page(all_pathes, image.size)
 
 
-def _generate_svgs(image_path_prefix):  
+def _generate_svg_pathes(image_id):  
+  # mkbitmap
+  #0 filter n: highpass filter
+  #1 scale n: scale the image with factor n
+  #2 linear: scale linear (fast) --cubic: scale cubic best results (slow) [l,c]
+  #3 threshold [0 .. 1]: the threshold grey value for bilevel conversion
+  #4 invert: invert the image [y,n]
+
+  # potrace
+  #5 turnpolicy [black, white, right, left, minority, majority, or random]
+  #6 turdsize n: suppress speckles of up to this many pixels
+  #7 alphamax n: how smooth the result is [-1 .. 1.334] default 1
+  #8 color #rrggbb: foreground color
+  #9 opaque background [y,n]
+  #10 fillcolor #rrggbb: background color
+
   all_pathes = []
-
-  #bmp_path = '{0:s}.bmp'.format(image_path_prefix)
-  #all_pathes.append(bmp_path)
-
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.35 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.40 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.45 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.47 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.50 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.52 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.55 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 4 --scale 2 --threshold 0.60 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 6 --scale 2 --threshold 0.50 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
-  all_pathes.append(_potrace('mkbitmap --filter 8 --scale 2 --threshold 0.50 | potrace --turdsize 5 --svg --alphamax 1', image_path_prefix))
+  all_thresholds = range(42,60,2)
+  for t in all_thresholds:
+    all_pathes.append('{0:s}/4-2-l-{1:d}-n-black-5-1-000000-n-FFFFFF.svg'.format(image_id, t))
   return all_pathes
 
 def _get_img_tag(src, size):
-  return '<img src="/{0:s}{1:s}" width="{2[0]:d}" height="{2[1]:d}" />'.format(relativ_upload_path, src, size)
-
-def _potrace(cmd, image_path_prefix):
-  # mkbitmap
-  # --filter n: highpass filter
-  # --scale n: scale the image with factor n
-  # --linear: scale linear (fast)
-  # --cubic: scale cubic best results (slow)
-  # --threshold [0 .. 1]: the threshold grey value for bilevel conversion
-  # --invert: invert the image
-
-  # potrace
-  # --svg: output format svg
-  # --turnpolicy [black, white, right, left, minority, majority, or random]
-  # --turdsize n: suppress speckles of up to this many pixels
-  # --alphamax n: how smooth the result is [-1 .. 1.334] default 1
-  # --scale n: scale image
-  # --color #rrggbb: foreground color
-  # --opaque --fillcolor #rrggbb: background color
-
-  suffix = cmd.replace(' ','').replace('--','-').replace('|','').replace('.','')
-  web_image_path = '{0:s}-{1:s}.svg'.format(image_path_prefix, suffix)
-  subprocess.call('cat {0:s}.bmp | {1:s} > {2:s}'.format(image_path_prefix, cmd, web_image_path), shell=True)
-  return web_image_path
+  return '<img src="/img/{0:s}" width="{1[0]:d}" height="{1[1]:d}" />'.format(src, size)
 
 def _get_html_page(all_pathes, size):
   html = '<!doctype html><html><head><meta charset=utf-8><title>Vectorize</title></head><body>'
