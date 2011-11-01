57684bb378bd75c6107888e23574c3ba87e7a97a
Christoph Schaefer <cppschaefer@gmail.com>
2011-11-01 22:10:35 +0100

    slider added

--- js/settings.js
+++ js/settings.js
@@ -4,8 +4,18 @@ function showPreview() {
 }
 
 $(function() {
-
+  var input = $( "input[name='highpass_filter']" );
+  $("#highpass_filter").slider({
+    change: showPreview,
+    value: input.val(),
+    min: 1,
+    max: 100,
+    step: 1,
+		slide: function( event, ui ) {
+			input.val( ui.value );
+		}
+  });
   $(":radio").click(showPreview);
   $("select").change(showPreview);
-  $("input").change(showPreview);
+  //$("input").change(showPreview);
 });
